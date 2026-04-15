4616f597e07d1e705f7691871587642442b77a2b
Omar <95233527+omar94khan@users.noreply.github.com>
2026-04-15 22:54:41 +0300

    Removing comments

--- app.py
+++ app.py
@@ -195,22 +195,13 @@ def main():
     disbursement_date = st.date_input(label = "Please define the Disbursement Date.")
     disbursement_date_string = str(disbursement_date).replace("-","")   
     EMI = st.number_input(label = "Please input the EMI", min_value = 0.001, format="%0.3f", step = 0.001, value = 100.000)
-    TakafulFactor = (st.number_input(label = "Please input the Takaful Rate in Monthly Percentage terms", min_value = 0.00, format="%0.2f", value = 5.5))/100
+    TakafulFactor = (st.number_input(label = "Please input the Takaful Rate in Monthly Percentage terms", min_value = 0.00, format="%0.2f", value = 0.00))/100
     GracePeriodMonths = st.number_input(label = "Please input the Grace Period in months", min_value = 0, max_value = 36, value = 3, step = 1)
 
     generate = st.button(label = "Generate Repayment Schedule")
     
     if generate:
         st.write("Generating Repayment Schedule...")
-        st.write("Parameters selected:")
-        st.write(f"Finance Amount: {FinanceAmount}")
-        st.write(f"Profit Rate: {ProfitRate}")
-        st.write(f"Payday: {payday}")
-        st.write(f"Disbursement Date: {disbursement_date_string}")
-        st.write(f"EMI: {EMI}")
-        st.write(f"Takaful Factor: {TakafulFactor}")
-        st.write(f"Grace Period Months: {GracePeriodMonths}")
-        st.write(f"Repayment Method: Deferral")
         df_final = DeferralRPS(FinanceAmount = FinanceAmount,
                                               ProfitRate = ProfitRate,
                                               PayDay = payday,
